3148dbdf3a85c27a5705f99e47efb3916996c49d
Mathuti Sivamanogaran <you@example.com>
2023-05-03 12:05:58 +0200

    Add more pokemons to home page

---
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,15 @@
+interface Props {
+    name: string
+    id: string
+} 
+
+export const Pokemon = ({name, id}: Props) => {
+    let img: string = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+    return(
+    <>
+    <img src = {img} alt = {name}/> 
+    <p> Name = {name} </p>
+    <p> Number = {id} </p>
+    </>
+    )
+  }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,9 +1,10 @@
-import styles from "./Home.module.css"
+import styles from "./Home.module.css";
+import {Pokemon} from 'components/Pokemon';
 
 
 export const Home = () => {
   return (
-    <>
+    <div className={styles.intro}>
     <div>Bienvenue sur ton futur pokédex !</div>
     <div>
       Tu vas pouvoir apprendre tout ce qu'il faut sur React, Redux et Symfony, et attraper des
@@ -11,10 +12,10 @@ export const Home = () => {
     </div>
     <div>Commence par créer ton premier pokemon: </div>
     <div> 
-    <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" alt = "Carapuce"/> 
-    <p>Name: Carapuce</p>
-    <p> Number: 7</p>
+    <Pokemon name= "Tortank" id = "7"/>
+    <Pokemon name= "Carabaffe" id = "8"/>
+    <Pokemon name= "Tortank" id = "9"/>
     </div>
-    </>
+    </div>
   );
   }
