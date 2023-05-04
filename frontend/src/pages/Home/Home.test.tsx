861a52cf5b22c8a19ef7936d39eeb93cd58f03d9
Mathuti Sivamanogaran <you@example.com>
2023-05-04 14:04:16 +0200

    add msw and run tests for API call

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -1,11 +1,41 @@
 import { render, screen } from "@testing-library/react"
 import { Home } from "./index"
+import { rest } from "msw"
+import { setupServer } from "msw/node"
+
+const server = setupServer(
+  rest.get("http://localhost:8000/pokemon", (req, res, ctx) => {
+    // Respond with a mocked user token that gets persisted
+    // in the `sessionStorage` by the `Login` component.
+    return res(
+      ctx.json([
+        { id: 1, name: "bulbasaur", height: 7, weight: 69 },
+        { id: 2, name: "ivysaur", height: 10, weight: 130 },
+      ]),
+    )
+  }),
+)
 
 describe("<Home />", () => {
-	it("should display Carapuce", ()=>{
+// Enable API mocking before tests.
+beforeAll(() => server.listen())
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers())
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close())
+
+	it("should display bulbasaur", async ()=>{
 		render(<Home />)
-		const carapuce = screen.getByText("Name: Carapuce")
-		expect(carapuce).toBeInTheDocument()
+		const bulbasaur = await screen.findByText("Name = bulbasaur")
+		expect(bulbasaur).toBeInTheDocument()
+  })
+
+  it("should display ivysaur", async ()=>{
+		render(<Home />)
+		const ivysaur = await screen.findByText("Name = ivysaur")
+		expect(ivysaur).toBeInTheDocument()
 })
 }
 )
